fe7ee6e26912d1c804ef38bf154af2ff14178192
Jared Matta <cherokeedreemer@gmail.com>
2024-05-29 20:51:48 +0000

    why wont this run

--- character.py
+++ character.py
@@ -1,8 +1,10 @@
+# WEAPON CLASS
 import pygame
 import math
 import weapon
 import constants
 
+# WEAPON CLASS
 class Character():
   def __init__(self, x, y, health, mob_animations, char_type, boss, size):
     self.char_type = char_type
@@ -24,7 +26,8 @@ class Character():
     self.image = self.animation_list[self.action][self.frame_index]
     self.rect = pygame.Rect(0, 0, constants.TILE_SIZE * size, constants.TILE_SIZE * size)
     self.rect.center = (x, y)
-
+    
+  # MOVE CLASS
   def move(self, dx, dy, obstacle_tiles, exit_tile = None):
     screen_scroll = [0, 0]
     level_complete = False
@@ -41,7 +44,7 @@ class Character():
       dx = dx * (math.sqrt(2)/2)
       dy = dy * (math.sqrt(2)/2)
 
-    #check for collision with map in x direction
+    # CHECK FOR COLLISIONS WITH MAP IN X DIRECTION
     self.rect.x += dx
     for obstacle in obstacle_tiles:
       #check for collision
@@ -52,29 +55,28 @@ class Character():
         if dx < 0:
           self.rect.left = obstacle[1].right
 
-    #check for collision with map in y direction
+    # CHECK FOR COLLISION WITH MAP IN THE Y DIRECTION
     self.rect.y += dy
     for obstacle in obstacle_tiles:
-      #check for collision
+      # CHECK FOR COLLLISION
       if obstacle[1].colliderect(self.rect):
-        #check which side the collision is from
+        # CHECK WHICH SIDE THE COLLISION COMES FROM
         if dy > 0:
           self.rect.bottom = obstacle[1].top
         if dy < 0:
           self.rect.top = obstacle[1].bottom
 
-
-    #logic only applicable to player
+    # LOGIC ONLY APPLICABLE TO PLAYER
     if self.char_type == 0:
-      #check collision with exit ladder
+      # CHECK COLLISION WITH EXIT LADDER
       if exit_tile[1].colliderect(self.rect):
-        #ensure player is close to the center of the exit ladder
+        # ENSURE PLAYER IS CLOSE TO THE CENTER OF THE EXIT LADDER
         exit_dist = math.sqrt(((self.rect.centerx - exit_tile[1].centerx) ** 2) + ((self.rect.centery - exit_tile[1].centery) ** 2))
         if exit_dist < 20:
           level_complete = True
 
-      #update scroll based on player position
-      #move camera left and right
+      # UPDATE SCROLL BASED ON PLAYER POSITION
+      # MOVE CAMERA LEFT AND RIGHT
       if self.rect.right > (constants.SCREEN_WIDTH - constants.SCROLL_THRESH):
         screen_scroll[0] = (constants.SCREEN_WIDTH - constants.SCROLL_THRESH) - self.rect.right
         self.rect.right = constants.SCREEN_WIDTH - constants.SCROLL_THRESH
@@ -82,7 +84,7 @@ class Character():
         screen_scroll[0] = constants.SCROLL_THRESH - self.rect.left
         self.rect.left = constants.SCROLL_THRESH
 
-      #move camera up and down
+      # MOVE CAMERA UP AND DOWN
       if self.rect.bottom > (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH):
         screen_scroll[1] = (constants.SCREEN_HEIGHT - constants.SCROLL_THRESH) - self.rect.bottom
         self.rect.bottom = constants.SCREEN_HEIGHT - constants.SCROLL_THRESH
@@ -100,18 +102,18 @@ class Character():
     ai_dy = 0
     fireball = None
 
-    #reposition the mobs based on screen scroll
+    # REPOSITION MOBS BASED ON THE SCREEN SCROLL
     self.rect.x += screen_scroll[0]
     self.rect.y += screen_scroll[1]
 
-    #create a line of sight from the enemy to the player
+    # CREATE A LINE OF SIGHT FROM THE ENEMY TO THE PLAYER
     line_of_sight = ((self.rect.centerx, self.rect.centery), (player.rect.centerx, player.rect.centery))
-    #check if line of sight passes through an obstacle tile
+    # CHECK IF THE LINE OF SIGHT PASSES THROUGH AN OBSTACLE TILE
     for obstacle in obstacle_tiles:
       if obstacle[1].clipline(line_of_sight):
         clipped_line = obstacle[1].clipline(line_of_sight)
 
-    #check distance to player
+    # CHECK DISTANCE TO PLAYER
     dist = math.sqrt(((self.rect.centerx - player.rect.centerx) ** 2) + ((self.rect.centery - player.rect.centery) ** 2))
     if not clipped_line and dist > constants.RANGE:
       if self.rect.centerx > player.rect.centerx:
@@ -125,14 +127,14 @@ class Character():
 
     if self.alive:
       if not self.stunned:
-        #move towards player
+        # MOVE TOWARDS PLAYER
         self.move(ai_dx, ai_dy, obstacle_tiles)
-        #attack player
+        # ATTACK PLAYER
         if dist < constants.ATTACK_RANGE and player.hit == False:
           player.health -= 10
           player.hit = True
           player.last_hit = pygame.time.get_ticks()
-        #boss enemies shoot fireballs
+        # BOSS ENEMIES SHOOT FIREBALLS
         fireball_cooldown = 700
         if self.boss:
           if dist < 500:
@@ -140,8 +142,7 @@ class Character():
               fireball = weapon.Fireball(fireball_image, self.rect.centerx, self.rect.centery, player.rect.centerx, player.rect.centery)
               self.last_attack = pygame.time.get_ticks()
 
-
-      #check if hit
+      # CHECK IF HIT
       if self.hit == True:
         self.hit = False
         self.last_hit = pygame.time.get_ticks()
@@ -155,26 +156,26 @@ class Character():
     return fireball
 
   def update(self):
-    #check if character has died
+    # CHECK IF THE PLAYER HAS DIED
     if self.health <= 0:
       self.health = 0
       self.alive = False
 
-    #timer to reset player taking a hit
+    # TIMER TO RESET PLAYER TAKING A HIT
     hit_cooldown = 1000
     if self.char_type == 0:
       if self.hit == True and (pygame.time.get_ticks() - self.last_hit) > hit_cooldown:
         self.hit = False
 
-    #check what action the player is performing
+    # CHECK WHAT ACTION THE PLAYER IS PERFORMING
     if self.running == True:
-      self.update_action(1)#1:run
+      self.update_action(1)#1: RUN
     else:
-      self.update_action(0)#0:idle
+      self.update_action(0)#0: IDLE
 
     animation_cooldown = 70
-    #handle animation
-    #update image
+    # HANDLE ANIMATION
+    # UPDATE IMAGE
     self.image = self.animation_list[self.action][self.frame_index]
     #check if enough time has passed since the last update
     if pygame.time.get_ticks() - self.update_time > animation_cooldown:
@@ -186,10 +187,10 @@ class Character():
 
 
   def update_action(self, new_action):
-    #check if the new action is different to the previous one
+    # CHECK IF THE NEW ACTION IS DIFFERENT TO THE PREVIOUS ONE
     if new_action != self.action:
       self.action = new_action
-      #update the animation settings
+      # UPDATE THE ANIMATION SETTINGS
       self.frame_index = 0
       self.update_time = pygame.time.get_ticks()
 
--- main.py
+++ main.py
@@ -13,7 +13,8 @@ mixer.init()
 pygame.init()
 
 # FIX  MAIN GAME SCREEN AND DISPLAY NAME
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+screen = pygame.display.set_mode(
+    (constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon Crawler")
 
 # CREATE CLOCK FOR MAINTIANING FRAME RATE
@@ -35,11 +36,13 @@ moving_down = False
 # DEFINE FONT
 font = pygame.font.Font("assets/fonts/AtariClassic.ttf", 20)
 
+
 # HELPER FUNCTION TO SCALE THE IMAGES
 def scale_img(image, scale):
   w = image.get_width()
   h = image.get_height()
   return pygame.transform.scale(image, (w * scale, h * scale))
+
 
 # LOAD AUDIO
 pygame.mixer.music.load("assets/audio/music.wav")
@@ -55,54 +58,87 @@ heal_fx = pygame.mixer.Sound("assets/audio/heal.wav")
 heal_fx.set_volume(0.5)
 
 # LOAD BUTTON IMAGES
-start_img = scale_img(pygame.image.load("assets/images/buttons/button_start.png").convert_alpha(), constants.BUTTON_SCALE)
-exit_img = scale_img(pygame.image.load("assets/images/buttons/button_exit.png").convert_alpha(), constants.BUTTON_SCALE)
-restart_img = scale_img(pygame.image.load("assets/images/buttons/button_restart.png").convert_alpha(), constants.BUTTON_SCALE)
-resume_img = scale_img(pygame.image.load("assets/images/buttons/button_resume.png").convert_alpha(), constants.BUTTON_SCALE)
+start_img = scale_img(
+    pygame.image.load(
+        "assets/images/buttons/button_start.png").convert_alpha(),
+    constants.BUTTON_SCALE)
+exit_img = scale_img(
+    pygame.image.load("assets/images/buttons/button_exit.png").convert_alpha(),
+    constants.BUTTON_SCALE)
+restart_img = scale_img(
+    pygame.image.load(
+        "assets/images/buttons/button_restart.png").convert_alpha(),
+    constants.BUTTON_SCALE)
+resume_img = scale_img(
+    pygame.image.load(
+        "assets/images/buttons/button_resume.png").convert_alpha(),
+    constants.BUTTON_SCALE)
 
 # LOAD HEART IMAGES
-heart_empty = scale_img(pygame.image.load("assets/images/items/heart_empty.png").convert_alpha(), constants.ITEM_SCALE)
-heart_half = scale_img(pygame.image.load("assets/images/items/heart_half.png").convert_alpha(), constants.ITEM_SCALE)
-heart_full = scale_img(pygame.image.load("assets/images/items/heart_full.png").convert_alpha(), constants.ITEM_SCALE)
+heart_empty = scale_img(
+    pygame.image.load("assets/images/items/heart_empty.png").convert_alpha(),
+    constants.ITEM_SCALE)
+heart_half = scale_img(
+    pygame.image.load("assets/images/items/heart_half.png").convert_alpha(),
+    constants.ITEM_SCALE)
+heart_full = scale_img(
+    pygame.image.load("assets/images/items/heart_full.png").convert_alpha(),
+    constants.ITEM_SCALE)
 
-#load coin images
+# LOAD COIN IMAGES
 coin_images = []
 for x in range(4):
-  img = scale_img(pygame.image.load(f"assets/images/items/coin_f{x}.png").convert_alpha(), constants.ITEM_SCALE)
+  img = scale_img(
+      pygame.image.load(f"assets/images/items/coin_f{x}.png").convert_alpha(),
+      constants.ITEM_SCALE)
   coin_images.append(img)
 
-#load potion image
-red_potion = scale_img(pygame.image.load("assets/images/items/potion_red.png").convert_alpha(), constants.POTION_SCALE)
+# LOAD POTION IMAGE
+red_potion = scale_img(
+    pygame.image.load("assets/images/items/potion_red.png").convert_alpha(),
+    constants.POTION_SCALE)
 
 item_images = []
 item_images.append(coin_images)
 item_images.append(red_potion)
 
-#load weapon images
-bow_image = scale_img(pygame.image.load("assets/images/weapons/bow.png").convert_alpha(), constants.WEAPON_SCALE)
-arrow_image = scale_img(pygame.image.load("assets/images/weapons/arrow.png").convert_alpha(), constants.WEAPON_SCALE)
-fireball_image = scale_img(pygame.image.load("assets/images/weapons/fireball.png").convert_alpha(), constants.FIREBALL_SCALE)
+# LOAD WEPAON IMAGES
+bow_image = scale_img(
+    pygame.image.load("assets/images/weapons/bow.png").convert_alpha(),
+    constants.WEAPON_SCALE)
+arrow_image = scale_img(
+    pygame.image.load("assets/images/weapons/arrow.png").convert_alpha(),
+    constants.WEAPON_SCALE)
+fireball_image = scale_img(
+    pygame.image.load("assets/images/weapons/fireball.png").convert_alpha(),
+    constants.FIREBALL_SCALE)
 
-#load tilemap images
+# LOAD TILEMAP IMAGES
 tile_list = []
 for x in range(constants.TILE_TYPES):
-  tile_image = pygame.image.load(f"assets/images/tiles/{x}.png").convert_alpha()
-  tile_image = pygame.transform.scale(tile_image, (constants.TILE_SIZE, constants.TILE_SIZE))
+  tile_image = pygame.image.load(
+      f"assets/images/tiles/{x}.png").convert_alpha()
+  tile_image = pygame.transform.scale(
+      tile_image, (constants.TILE_SIZE, constants.TILE_SIZE))
   tile_list.append(tile_image)
 
-#load character images
+# LOAD CHARACTER IMAGES
 mob_animations = []
-mob_types = ["elf", "imp", "skeleton", "goblin", "muddy", "tiny_zombie", "big_demon"]
+mob_types = [
+    "elf", "imp", "skeleton", "goblin", "muddy", "tiny_zombie", "big_demon"
+]
 
 animation_types = ["idle", "run"]
 for mob in mob_types:
-  #load images
+  # LOAD IMAGES
   animation_list = []
   for animation in animation_types:
-    #reset temporary list of images
+    # RESET TEMPORARY LIST OF IMAGES
     temp_list = []
     for i in range(4):
-      img = pygame.image.load(f"assets/images/characters/{mob}/{animation}/{i}.png").convert_alpha()
+      img = pygame.image.load(
+          f"assets/images/characters/{mob}/{animation}/{i}.png").convert_alpha(
+          )
       img = scale_img(img, constants.SCALE)
       temp_list.append(img)
     animation_list.append(temp_list)
@@ -114,10 +150,12 @@ def draw_text(text, font, text_col, x, y):
   img = font.render(text, True, text_col)
   screen.blit(img, (x, y))
 
+
 #function for displaying game info
 def draw_info():
   pygame.draw.rect(screen, constants.PANEL, (0, 0, constants.SCREEN_WIDTH, 50))
-  pygame.draw.line(screen, constants.WHITE, (0, 50), (constants.SCREEN_WIDTH, 50))
+  pygame.draw.line(screen, constants.WHITE, (0, 50),
+                   (constants.SCREEN_WIDTH, 50))
   #draw lives
   half_heart_drawn = False
   for i in range(5):
@@ -130,9 +168,12 @@ def draw_info():
       screen.blit(heart_empty, (10 + i * 50, 0))
 
   #level
-  draw_text("LEVEL: " + str(level), font, constants.WHITE, constants.SCREEN_WIDTH / 2, 15)
+  draw_text("LEVEL: " + str(level), font, constants.WHITE,
+            constants.SCREEN_WIDTH / 2, 15)
   #show score
-  draw_text(f"X{player.score}", font, constants.WHITE, constants.SCREEN_WIDTH - 100, 15)
+  draw_text(f"X{player.score}", font, constants.WHITE,
+            constants.SCREEN_WIDTH - 100, 15)
+
 
 #function to reset level
 def reset_level():
@@ -150,9 +191,9 @@ def reset_level():
   return data
 
 
-
 #damage text class
 class DamageText(pygame.sprite.Sprite):
+
   def __init__(self, x, y, damage, color):
     pygame.sprite.Sprite.__init__(self)
     self.image = font.render(damage, True, color)
@@ -172,8 +213,10 @@ class DamageText(pygame.sprite.Sprite):
     if self.counter > 30:
       self.kill()
 
+
 #class for handling screen fade
 class ScreenFade():
+
   def __init__(self, direction, colour, speed):
     self.direction = direction
     self.colour = colour
@@ -183,13 +226,22 @@ class ScreenFade():
   def fade(self):
     fade_complete = False
     self.fade_counter += self.speed
-    if self.direction == 1:#whole screen fade
-      pygame.draw.rect(screen, self.colour, (0 - self.fade_counter, 0, constants.SCREEN_WIDTH // 2, constants.SCREEN_HEIGHT))
-      pygame.draw.rect(screen, self.colour, (constants.SCREEN_WIDTH // 2 + self.fade_counter, 0, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-      pygame.draw.rect(screen, self.colour, (0, 0 - self.fade_counter, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT // 2))
-      pygame.draw.rect(screen, self.colour, (0, constants.SCREEN_HEIGHT // 2 + self.fade_counter, constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    elif self.direction == 2:#vertical screen fade down
-      pygame.draw.rect(screen, self.colour, (0, 0, constants.SCREEN_WIDTH, 0 + self.fade_counter))
+    if self.direction == 1:  #whole screen fade
+      pygame.draw.rect(screen, self.colour,
+                       (0 - self.fade_counter, 0, constants.SCREEN_WIDTH // 2,
+                        constants.SCREEN_HEIGHT))
+      pygame.draw.rect(screen, self.colour,
+                       (constants.SCREEN_WIDTH // 2 + self.fade_counter, 0,
+                        constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+      pygame.draw.rect(screen, self.colour,
+                       (0, 0 - self.fade_counter, constants.SCREEN_WIDTH,
+                        constants.SCREEN_HEIGHT // 2))
+      pygame.draw.rect(screen, self.colour,
+                       (0, constants.SCREEN_HEIGHT // 2 + self.fade_counter,
+                        constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    elif self.direction == 2:  #vertical screen fade down
+      pygame.draw.rect(screen, self.colour,
+                       (0, 0, constants.SCREEN_WIDTH, 0 + self.fade_counter))
 
     if self.fade_counter >= constants.SCREEN_WIDTH:
       fade_complete = True
@@ -204,11 +256,10 @@ for row in range(constants.ROWS):
   world_data.append(r)
 #load in level data and create world
 with open(f"levels/level{level}_data.csv", newline="") as csvfile:
-  reader = csv.reader(csvfile, delimiter = ",")
+  reader = csv.reader(csvfile, delimiter=",")
   for x, row in enumerate(reader):
     for y, tile in enumerate(row):
       world_data[x][y] = int(tile)
-
 
 world = World()
 world.process_data(world_data, tile_list, item_images, mob_animations)
@@ -233,16 +284,19 @@ item_group.add(score_coin)
 for item in world.item_list:
   item_group.add(item)
 
-
 #create screen fades
 intro_fade = ScreenFade(1, constants.BLACK, 4)
 death_fade = ScreenFade(2, constants.PINK, 4)
 
 #create button
-start_button = Button(constants.SCREEN_WIDTH // 2 - 145, constants.SCREEN_HEIGHT // 2 - 150, start_img)
-exit_button = Button(constants.SCREEN_WIDTH // 2 - 110, constants.SCREEN_HEIGHT // 2 + 50, exit_img)
-restart_button = Button(constants.SCREEN_WIDTH // 2 - 175, constants.SCREEN_HEIGHT // 2 - 50, restart_img)
-resume_button = Button(constants.SCREEN_WIDTH // 2 - 175, constants.SCREEN_HEIGHT // 2 - 150, resume_img)
+start_button = Button(constants.SCREEN_WIDTH // 2 - 145,
+                      constants.SCREEN_HEIGHT // 2 - 150, start_img)
+exit_button = Button(constants.SCREEN_WIDTH // 2 - 110,
+                     constants.SCREEN_HEIGHT // 2 + 50, exit_img)
+restart_button = Button(constants.SCREEN_WIDTH // 2 - 175,
+                        constants.SCREEN_HEIGHT // 2 - 50, restart_img)
+resume_button = Button(constants.SCREEN_WIDTH // 2 - 175,
+                       constants.SCREEN_HEIGHT // 2 - 150, resume_img)
 
 #main game loop
 run = True
@@ -282,12 +336,15 @@ while run:
           dy = constants.SPEED
 
         #move player
-        screen_scroll, level_complete = player.move(dx, dy, world.obstacle_tiles, world.exit_tile)
+        screen_scroll, level_complete = player.move(dx, dy,
+                                                    world.obstacle_tiles,
+                                                    world.exit_tile)
 
         #update all objects
         world.update(screen_scroll)
         for enemy in enemy_list:
-          fireball = enemy.ai(player, world.obstacle_tiles, screen_scroll, fireball_image)
+          fireball = enemy.ai(player, world.obstacle_tiles, screen_scroll,
+                              fireball_image)
           if fireball:
             fireball_group.add(fireball)
           if enemy.alive:
@@ -298,9 +355,11 @@ while run:
           arrow_group.add(arrow)
           shot_fx.play()
         for arrow in arrow_group:
-          damage, damage_pos = arrow.update(screen_scroll, world.obstacle_tiles, enemy_list)
+          damage, damage_pos = arrow.update(screen_scroll,
+                                            world.obstacle_tiles, enemy_list)
           if damage:
-            damage_text = DamageText(damage_pos.centerx, damage_pos.y, str(damage), constants.RED)
+            damage_text = DamageText(damage_pos.centerx, damage_pos.y,
+                                     str(damage), constants.RED)
             damage_text_group.add(damage_text)
             hit_fx.play()
         damage_text_group.update()
@@ -329,7 +388,7 @@ while run:
         world_data = reset_level()
         #load in level data and create world
         with open(f"levels/level{level}_data.csv", newline="") as csvfile:
-          reader = csv.reader(csvfile, delimiter = ",")
+          reader = csv.reader(csvfile, delimiter=",")
           for x, row in enumerate(reader):
             for y, tile in enumerate(row):
               world_data[x][y] = int(tile)
@@ -341,12 +400,12 @@ while run:
         player.health = temp_hp
         player.score = temp_score
         enemy_list = world.character_list
-        score_coin = Item(constants.SCREEN_WIDTH - 115, 23, 0, coin_images, True)
+        score_coin = Item(constants.SCREEN_WIDTH - 115, 23, 0, coin_images,
+                          True)
         item_group.add(score_coin)
         #add the items from the level data
         for item in world.item_list:
           item_group.add(item)
-
 
       #show intro
       if start_intro == True:
@@ -363,17 +422,19 @@ while run:
             world_data = reset_level()
             #load in level data and create world
             with open(f"levels/level{level}_data.csv", newline="") as csvfile:
-              reader = csv.reader(csvfile, delimiter = ",")
+              reader = csv.reader(csvfile, delimiter=",")
               for x, row in enumerate(reader):
                 for y, tile in enumerate(row):
                   world_data[x][y] = int(tile)
             world = World()
-            world.process_data(world_data, tile_list, item_images, mob_animations)
+            world.process_data(world_data, tile_list, item_images,
+                               mob_animations)
             temp_score = player.score
             player = world.player
             player.score = temp_score
             enemy_list = world.character_list
-            score_coin = Item(constants.SCREEN_WIDTH - 115, 23, 0, coin_images, True)
+            score_coin = Item(constants.SCREEN_WIDTH - 115, 23, 0, coin_images,
+                              True)
             item_group.add(score_coin)
             #add the items from the level data
             for item in world.item_list:
@@ -396,7 +457,6 @@ while run:
       if event.key == pygame.K_ESCAPE:
         pause_game = True
 
-
     #keyboard button released
     if event.type == pygame.KEYUP:
       if event.key == pygame.K_a:
@@ -409,6 +469,5 @@ while run:
         moving_down = False
 
   pygame.display.update()
-
 
 pygame.quit()
